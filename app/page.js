d2a194da3819e52ad472dda797ccff5fb305ed90
Tyron Gracias <tyrongracias@gmail.com>
2026-08-17 14:31:56 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 'use client';
 
-import { useEffect, useRef, useState } from 'react';
-
+import { useEffect, useRef } from 'react';
+import Navbar from './components/Navbar';
 const services = [
   {
     id: '01',
